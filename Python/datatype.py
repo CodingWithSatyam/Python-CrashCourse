2feed400a9bbb96dc04ac4aa8b8e5f096d4824cb
CodingWithSatyam <100499761+CodingWithSatyam@users.noreply.github.com>
2022-03-26 07:05:09 +0545

    Add files via upload

---
+++ Python/datatype.py
@@ -0,0 +1,30 @@
+# types of data type
+
+# 1. string
+# 2. numeric
+# 3. sequesnce
+# 4. set
+# 5. Boolean
+
+# String
+
+# a = "Satyam Mishra"
+
+# print(type(a))
+
+# Numeric
+
+# 1. int
+# 2. float
+# 3. complex
+
+# x = 1 # int
+# y = 2.8 # float
+# z = 1j # complex
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+# a = 2
+# print(type(a))
---
+++ Python/main.py
@@ -0,0 +1,24 @@
+# print("Hello World")
+
+# x = 5
+# a = "Coding With Satyam"
+
+# print(a)
+
+# x = 5
+
+# carName = 'Tesla'
+
+# print(x, carName)
+
+# Variables Rules
+
+# x =5
+
+# _ = 54
+
+# age = 10
+# Age = 12
+
+# print(Age)
+# print(age)
